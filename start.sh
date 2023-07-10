5c8389dc095726055f510c9084318e308c92fb71
adityarise <134573036+adityarise@users.noreply.github.com>
2023-07-10 18:30:45 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Srikanth5657/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
+git clone https://github.com/adityarise/tU-Link-Shortener
 cd /Shortner-Converter-Bot-V2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
